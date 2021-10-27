2f5125dff81b9960f44d29fa740743404ffce489
l1dl1d <javeekh@ssfs.org>
2021-10-27 09:01:08 -0400

    almost done unit 5 assignment

--- assignment_five.py
+++ assignment_five.py
@@ -1,11 +1,20 @@
 import random
-def get_number():
-    x = input("guess a number")
-    if x == num:
-        print("congratulations you guessed the right number")
-    else:
-        print("try again")
 def get_guess():
     num = random.randint(1, 100)
     return num
+def get_number():
+    num = str(random.randint(1, 100))
+    while True:
+        x = str(input("guess a number"))
+        if x == num:
+            print("congratulations you guessed the right number")
+            break
+        elif x > num:
+            print("number was too high")
+        else:
+            print("number was too low")
 def main():
+    get_guess()
+    print(get_number())
+if __name__ == '__main__':
+    main()
