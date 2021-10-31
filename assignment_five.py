dafed4b1e854cbaf0ee51bc0093bfd517414ba8c
kjaveed <javeekh@ssfs.org>
2021-10-30 21:14:07 -0400

    finished assignment 5

--- assignment_five.py
+++ assignment_five.py
@@ -1,20 +1,46 @@
+#Khalid
+#10/30/21
+#This program plays a guessing game that which generates a random number for the player to guess.
+#There are three games and after every game the program gives how many tries it took to guess the random number.
+#At the end the program gives you the average of the total amount of guess from all the games together.
 import random
-def get_guess():
+def get_number():
+    """
+    this function gets a random number and stores it in the variable num
+    :return:
+    """
     num = random.randint(1, 100)
     return num
-def get_number():
-    num = str(random.randint(1, 100))
+
+def get_guess():
+    """
+    this function prevents players from going outside of the 1 to 100 range when guessing a number
+    :return:
+    """
     while True:
-        x = str(input("guess a number"))
-        if x == num:
-            print("congratulations you guessed the right number")
-            break
-        elif x > num:
-            print("number was too high")
-        else:
-            print("number was too low")
+        x = str(input("guess a number between one and one hundred"))
+        if int(x) < 100 and int(x) > 1:
+            return x
 def main():
-    get_guess()
-    print(get_number())
+    """
+    this function runs the entire game and calls all the other functions to put everything together
+    :return:
+    """
+    guesses_for_all_games = 0
+    for y in range(3):
+        total_guesses = 0
+        num = get_number()
+        while True:
+            x = get_guess()
+            total_guesses = total_guesses + 1
+            guesses_for_all_games = guesses_for_all_games + 1
+            if int(x) == num:
+                print("congratulations you guessed the right number, it took you", total_guesses, "tries to get the answer")
+                break
+            elif int(x) > num:
+                print("number was too high")
+            else:
+                print("number was too low")
+    print("your average out of all three games was", guesses_for_all_games/3)
 if __name__ == '__main__':
     main()
